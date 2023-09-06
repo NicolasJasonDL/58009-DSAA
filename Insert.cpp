dd185cd5840f411ebb887f9dcedb570f2ccca93a
NicolasJasonDL <123909279+NicolasJasonDL@users.noreply.github.com>
2023-09-06 17:53:46 +0800

    Add files via upload

---
+++ Insert.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+int main()
+{
+	
+    vector<int> num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+	int x = 27;
+ 
+    num.insert(num.begin(), x);
+ 
+    for (int &i: num) 
+	{
+        cout << i << " ";
+    }
+ 
+    return 0;
+}
